0745ccac980e28febb94068afa2eeff4ebfd3238
The Dance <thedance@amamu.io>
2025-02-11 10:21:24 +0000

    add SSV configuration and MEV configuration

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -7,7 +7,7 @@ sudo ufw default allow outgoing
 
 sudo ufw allow 22/tcp comment 'Allow SSH port'
 
-sudo ufw allow 30303 comment 'Allow execution client port'
+sudo ufw allow 30303 comment 'Allow execution cleint port'
 
 sudo ufw allow 9000 comment 'Allow consensus client port'
 
@@ -28,3 +28,14 @@ sudo mkdir -p /secrets
 openssl rand -hex 32 | tr -d "\n" | sudo tee /secrets/jwtsecret
 
 sudo chmod 644 /secrets/jwtsecret
+
+#install docker and docker-compose
+apt-get update
+apt-get install -y docker.io
+apt-get install -y docker-compose
+
+#Manage Docker as a non-root user
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
--- ssv_configuration.sh
+++ ssv_configuration.sh
@@ -4,4 +4,4 @@
 echo "<MY_OPERATOR_PASSWORD>" >> password
 
 #Key Pair generation and encryption
-docker run --name ssv-node-key-generation -v <PATH_TO_PASSWORD>:/password -it "ssvlabs/ssv-node:latest" /go/bin/ssvnode generate-operator-keys --password-file=password && docker cp ssv-node-key-generation:/encrypted_private_key.json ./encrypted_private_key.json && docker rm ssv-node-key-generation
+docker run --name ssv-node-key-generation -v  ./password:/password -it "ssvlabs/ssv-node:latest" /go/bin/ssvnode generate-operator-keys --password-file=password && docker cp ssv-node-key-generation:/encrypted_private_key.json ./encrypted_private_key.json && docker rm ssv-node-key-generation
